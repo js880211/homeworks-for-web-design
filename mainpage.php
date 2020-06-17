54f1fd287c49c41144e23da7045df593cf53d43e
js880211 <personlostinanime@gmail.com>
2020-06-18 00:33:59 +0800

    1

--- mainpage.php
+++ mainpage.php
@@ -1,43 +1,42 @@
 <html>
-    <head>
-        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-        <title>WWW WEB Design</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <div id="loading">
-          <H1>Now loading...</H1>
-        </div>
-        <nav class="navbar bg-dark navbar-dark">
-            <!-- Brand -->
-            <a class="navbar-brand" href="#">網頁連結</a>
-            <!-- Toggler/collapsibe Button -->
-            <button class="navbar-toggler bg-primary" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <!-- Navbar links -->
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul class="navbar-nav">
+
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <title>WWW WEB Design</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div id="loading">
+        <H1>Now loading...</H1>
+    </div>
+    <nav class="navbar bg-dark navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">網頁連結</a>
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler bg-primary" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                  <a class="nav-link" href="./threejs/mainscene.html">threejs</a>
-                  <a class="nav-link" href="./gallery/index.html">gallery</a>
+                    <a class="nav-link" href="./threejs/mainscene.html">threejs</a>
+                    <a class="nav-link" href="./gallery/index.html">gallery</a>
                 </li>
-              </ul>
-             
-            </div>
-          </nav>
-            <div class="container">
-              
-                
-                <h1 class="text-success">WEB DESIGN Main Page</h1>
-                <h3>全球資訊網頁設計 連結頁</h3>
-                <hr class="style1">
-                 <?php
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <h1 class="text-success">WEB DESIGN Main Page</h1>
+        <h3>全球資訊網頁設計 連結頁</h3>
+        <hr class="style1">
+        <?php
                 header("Content-Type:text/html; charset=utf-8");
                 $servername = "10.55.144.3";
                 $username = "root";
@@ -46,7 +45,7 @@
                         $conn = new PDO("mysql:host=$servername;dbname=AnimeTable;charset=utf8",$username,$password);
                         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
                         
-                        echo "<table class='table'> <tr> <th>名稱</th> <th>年分</th> <th>月份</th> </tr>";
+                        echo "<table class='table'> <tr> <th class='bg-primary'>名稱</th> <th>年分</th> <th>月份</th> </tr>";
                         $sql = "Select * from Anime";
                         $db=$conn->query($sql);
                         $dbs=$db->fetchAll(PDO::FETCH_NUM);
@@ -62,9 +61,8 @@
                 }
                 $conn = null;
                 ?>
-            </div>
-      
-        
+    </div>
     <script src="mainpage.js"></script>
-    </body>
+</body>
+
 </html>
