7de610f3cdc386f9da914ea558ee1ad89d7f25b3
js880211 <personlostinanime@gmail.com>
2020-06-17 17:52:07 +0800

    0

--- mainpage.php
+++ mainpage.php
@@ -50,7 +50,7 @@
                         
                         foreach($dbs as $row)
                         {
-                        echo $row[0];
+                        echo $row;
                         
                         }
                 }catch(PDOException $e){
