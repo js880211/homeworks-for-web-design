72fbd64c2f3c90bdefa94b6e83d839bddbe2109a
js880211 <personlostinanime@gmail.com>
2020-06-19 21:36:34 +0800

    1

--- mainpage.js
+++ mainpage.js
@@ -26,5 +26,5 @@ function finish(){
     
 }*/
 $(document).ready(function(){
-    $("#menu").html("<a class='dropdown-item' href='#'>Link 1</a>");
+    $("#menu").children().html("<a class='dropdown-item' href='#'>Link 1</a>");
 });
