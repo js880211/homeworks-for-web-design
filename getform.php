74c1fdad4c221a0375c93079ad7cdd766f4cce52
js880211 <personlostinanime@gmail.com>
2020-06-20 17:35:19 +0800

    1

--- getform.php
+++ getform.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <script>
+    <script
         async defer src = "https://apis.google.com/js/api.js"
         onload = "this.onload=function(){};handleClientLoad()"
         onreadystatechange = "if (this.readyState === 'complete') this.onload()" >
