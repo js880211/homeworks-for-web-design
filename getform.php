16a620a1127dd93317732c12946d999ff0994297
js880211 <personlostinanime@gmail.com>
2020-06-21 01:55:08 +0800

    1

--- getform.php
+++ getform.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 console.log(response.result.spreadsheetId);
                 var values = [
                     [
-                        "HI"
+                        "HI","OK","YOU"
                     ],
                     // Additional rows ...
                 ];
