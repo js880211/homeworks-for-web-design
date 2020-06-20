c3bba1b351cbebd9a4b54e3fad2abc6f5d5c7e48
js880211 <personlostinanime@gmail.com>
2020-06-20 17:33:47 +0800

    1

--- getform.php
+++ getform.php
@@ -2,26 +2,31 @@
 // define variables and set to empty values
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $names = $_POST["checkbox"];
-  foreach($names as $name){
-      echo $name."<br>";
-  }
+    $names = $_POST["checkbox"];
+    foreach ($names as $name) {
+        echo $name . "<br>";
+    }
 }
 
 
 ?>
 <html>
-    <head>
 
-    </head>
-    <body>
-        <script>
+<head>
+
+</head>
+
+<body>
+    <script>
+        async defer src = "https://apis.google.com/js/api.js"
+        onload = "this.onload=function(){};handleClientLoad()"
+        onreadystatechange = "if (this.readyState === 'complete') this.onload()" >
             gapi.client.sheets.spreadsheets.create({
-            properties: {
-                title: title
-            }
-            }).then((response) => {
-            });
-        </script>
-    </body>
+                properties: {
+                    title: title
+                }
+            }).then((response) => {});
+    </script>
+</body>
+
 </html>
