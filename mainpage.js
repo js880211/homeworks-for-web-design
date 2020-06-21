9a71e4a666dc2d001d8fe4f8338c4a5d474d172f
js880211 <personlostinanime@gmail.com>
2020-06-21 12:16:15 +0800

    1

--- mainpage.js
+++ mainpage.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
                 var num=value.slice(0,4);
                 $.get(`animedata.php?q=${num}`, function (data, status) {
                     {
-                        $("#cols").html("<input type='submit' name='submit' value='送出' id='sub'>");
+                        $("#cols").html("<div id='sub'><input type='submit' name='submit' value='送出'></div>");
                         $("#cols").append(data);
                     }
                 });
